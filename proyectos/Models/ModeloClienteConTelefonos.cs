92671648695dd3e65249deef542b458ea303af4c
johnsydev <johnsylopez02@gmail.com>
2025-06-09 01:03:35 -0600

    Avance en interfaz de registro de cliente
    Co-authored-by: Tayler1203 <tayler1203@gmail.com>

---
+++ proyectos/Models/ModeloClienteConTelefonos.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelesCaribe.Models
+{
+    public class ModeloClienteConTelefonos
+    {
+        public Cliente Cliente { get; set; } = new Cliente();
+
+        public string? Telefono1 { get; set; }
+        public string? Telefono2 { get; set; }
+        public string? Telefono3 { get; set; }
+    }
+}
